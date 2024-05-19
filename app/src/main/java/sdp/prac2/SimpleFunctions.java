a13a65be914f872b8fadd0fd7b40f32816e2b570
Mthobisi <mthobisibrillylatha@gmail.com>
2024-05-19 11:28:36 +0200

    Completed assigned task in SimpleFunctions.java

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -4,4 +4,19 @@ import java.util.*;
 
 public class SimpleFunctions {
     public SimpleFunctions() {}
+    
+    // Task 5
+    public static <T extends Comparable<T>> boolean isSorted(List<T> list) {
+        if (list == null || list.size() <= 1) {
+            return true; // An empty list or a list with one element is considered sorted
+        }
+
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (list.get(i).compareTo(list.get(i + 1)) > 0) {
+                return false; // If an element is greater than the next one, the list is not sorted
+            }
+        }
+        return true; // All elements are in ascending order
+    }
+
 }
