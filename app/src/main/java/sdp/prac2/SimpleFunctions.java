c04d5d78168cfe84e4220c2d06aa63655ab2dfff
Sello Prince Lebudi <g22l1730@campus.ru.ac.za>
2024-05-20 15:49:50 +0200

    Added method for Task 4

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -1,7 +1,20 @@
 package sdp.prac2;
-
 import java.util.*;
 
 public class SimpleFunctions {
     public SimpleFunctions() {}
+
+    public List<Integer> Task4 (List<Integer> a, List<Integer> b) {   
+        if (a.size() != b.size()) {   
+            return null;
+        }
+
+        List<Integer> finalResult = new ArrayList<>();
+        for (int i = 0; i < a.size(); i++) {
+            int product = a.get(i) * b.get(b.size() - 1 - i);
+            finalResult.add(product);
+        }
+
+        return finalResult;
+    }
 }
