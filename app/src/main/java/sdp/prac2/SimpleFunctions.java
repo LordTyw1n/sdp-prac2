783d52a8eb53a8271505b47c91ca1a49e5e8e28e
Usandisele Magxwalisa <G21M2223@ict.ru.ac.za>
2024-05-21 12:56:07 +0200

    Completed task 6

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -4,4 +4,26 @@ import java.util.*;
 
 public class SimpleFunctions {
     public SimpleFunctions() {}
+
+    public static List<Integer> tasksix(List<Integer> numbers){   //method name "tasksix"
+        List<Integer> roundednum = new ArrayList<>();     //A new arraylist is created
+        for (int num : numbers) {          //for loop goes through each number in provided integer list called "numbers"
+            roundednum.add(roundUp(num,100));  //this adds each new number that has been added to the new list
+        }
+        return roundednum;
+    }
+
+    private static int roundUp(int num, int divisor){
+        int remainder = num % divisor;
+
+        int roundedUp;     //new int variable
+        if (remainder > 0) {
+            roundedUp = num + (divisor - remainder) ;   //rounds up num to the highest multiple of divisor ex. roundednum = 213 + (100 - 13), roundednum = 300 which is divisible by 100
+        }else{
+            roundedUp = num;  //if number is divisble, 
+        }
+        return roundedUp;
+    }
+
 }
+
