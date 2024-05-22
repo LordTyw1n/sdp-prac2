e50ba409357e30626c92c664903487a6222592d9
okuhlemadida <111228251+okuhlemadida@users.noreply.github.com>
2024-05-22 20:54:52 +0200

    Test for Task4

--- app/src/test/java/sdp/prac2/AppTest.java
+++ app/src/test/java/sdp/prac2/AppTest.java
@@ -12,6 +12,23 @@ public class AppTest {
 <<<<<<< HEAD
 =======
 class AppTest {
+    @Test 
+    public void testTask4(){
+        List<Integer> x = List.of( 1 , 2 , 3 , 4 , 5);
+        List<Integer> y = List.of(5 , 4 , 3 , 2 , 1);
+        List<Integer> result = List.of(1 , 4, 9, 16, 25);
+        assertEquals(result , SimpleFunctions.Task4(x,y) , "Values should be correctly multiplied");
+
+    }
+
+    @Test 
+    public void testTask4_2(){
+        List<Integer> x = List.of(1,2,3);
+        List<Integer> b = List.of(1,3);
+        assertEquals(null , SimpleFunctions.Task4(x,b) ,"Lists can not be different sizes");
+
+    }
+
     @Test
     public void Task4_EqualSizeLists_CorrectResult() {
         // Arrange
