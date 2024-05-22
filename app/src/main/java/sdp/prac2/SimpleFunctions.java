96ec0b9d3e3f0566634719d3119d06810b147344
okuhlemadida <111228251+okuhlemadida@users.noreply.github.com>
2024-05-22 19:04:39 +0200

    Initial commitT3

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -3,5 +3,20 @@ package sdp.prac2;
 import java.util.*;
 
 public class SimpleFunctions {
+    public static boolean Task3(String x){
+        int count = 0;
+        for (int i = 0; i < x.length(); i++){
+            char c = x.charAt(i);   
+            if (c == '('){
+                count++;
+            }else if(c == ')'){
+                count--;
+            }
+            if(count < 0){
+                return false; //closing brackets > opening brackets
+            }
+        }
+        return count == 0; // opening brackets = closing brackets
+    }
     public SimpleFunctions() {}
 }
