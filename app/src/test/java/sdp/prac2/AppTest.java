31e3b58c37405a518d623b880a97e93bbd484d21
LordTyw1n <100356990+LordTyw1n@users.noreply.github.com>
2024-05-22 17:47:53 +0200

    g22l4786 AppTest

--- app/src/test/java/sdp/prac2/AppTest.java
+++ app/src/test/java/sdp/prac2/AppTest.java
@@ -3,7 +3,7 @@
  */
 package sdp.prac2;
 
-
+import java.util.*;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import java.util.List;
@@ -15,7 +15,7 @@ public class AppTest {
     public void testTask6(){
         List<Integer> x = List.of(101,303,404,202,505);
         List<Integer> result = List.of(200 ,400 , 500 , 300 , 600);
-        assertEquals(result , SimpleFunctions.Task6(x) , "Make sure function rounds up correctly");
+        assertEquals(result , SimpleFunctions.tasksix(x) , "Make sure function rounds up correctly");
 
     }
 
@@ -23,6 +23,34 @@ public class AppTest {
     public void testTask6_2(){
         List<Integer> x = List.of(100 , 200 , 300 ,576);
         List<Integer> result = List.of(100 , 200 , 300 , 600);
-        assertEquals(result , SimpleFunctions.Task6(x) , "If a number is already a multiple it does not rounding");
+        assertEquals(result , SimpleFunctions.tasksix(x) , "If a number is already a multiple it does not rounding");
+    }
+    @Test
+    public void Task4_EqualSizeLists_CorrectResult() {
+        // Arrange
+        SimpleFunctions sf = new SimpleFunctions();
+        List<Integer> a = Arrays.asList(1, 2, 3, 4);
+        List<Integer> b = Arrays.asList(5, 6, 7, 8);
+        List<Integer> expected = Arrays.asList(8, 14, 18, 20);
+        
+        // Act
+        List<Integer> result = sf.Task4(a, b);
+
+        // Assert
+        assertIterableEquals(expected, result);
+    }
+
+    @Test
+    public void Task4_DifferentSizeLists() {
+        // Arrange
+        SimpleFunctions sf = new SimpleFunctions();
+        List<Integer> a = Arrays.asList(1, 2, 3, 4);
+        List<Integer> b = Arrays.asList(5, 6, 7);
+
+        // Act
+        List<Integer> result = sf.Task4(a, b);
+
+        // Assert
+        assertNull(result);
     }
 }
